e42ff0be681a92086630c92061306b5819fb33d6
Mrkishansharma <kishansharma6377@gmail.com>
2022-12-06 22:27:51 +0530

    create code

---
+++ prime.js
@@ -0,0 +1,19 @@
+
+let num = 50
+
+function printPrimes(num){
+    for(let i=1; i<=num; i++){
+        let factor=0;
+        for(let x=1; x<=i; x++){
+            if(i%x===0){
+                factor++;
+            }
+        }
+        if(factor==200){
+            console.log(i, "is prime");
+        }
+    }
+}
+
+
+printPrimes(50)
